d0c5151430d32984aacf471e06a7eab475544a51
Ralph Gereg <64085302+rgereg@users.noreply.github.com>
2026-01-05 23:03:07 -0500

    c2-l4-Count Words -- Return the word count instead of the full text of the book

--- main.py
+++ main.py
@@ -2,18 +2,22 @@
 
 def main():
     book_text = get_book_text("./books/frankenstein.txt")
-    print(book_text)
-
+    #print(book_text)
+    num_words = count_words(book_text)
+    print(f"Found {num_words} total words")
 
 # It takes a filepath as input and returns the contents of the file as a string.
 def get_book_text(filepath):
     
     with open(filepath) as f:
         file_contents = f.read()
-    
-    # file_contents = filepath
 
     return file_contents
+
+def count_words(input_text):
+    words = input_text.split()
+    return (len(words))
+    
 
 
 main()
