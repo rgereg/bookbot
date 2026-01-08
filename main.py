4b9cdaded5841898802476cdc3962f94f26be40d
Ralph Gereg <64085302+rgereg@users.noreply.github.com>
2026-01-07 21:53:19 -0500

    interim save working on lesson cs-l6

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from stats import count_words
+from stats import count_chars
+book = "./books/frankenstein.txt"
+
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
-    #print(book_text)
+
+
+    book_text = get_book_text(book)
     num_words = count_words(book_text)
     print(f"Found {num_words} total words")
 
-
+    stats = count_chars(book)
+    
 def get_book_text(filepath):
     
     with open(filepath) as f:
         file_contents = f.read()
 
     return file_contents
-
-
-    
-
 
 main()
--- stats.py
+++ stats.py
@@ -1,5 +1,24 @@
 
-
+# Take an input string and return the number of words contained
 def count_words(input_text):
     words = input_text.split()
     return (len(words))
+
+# takes the text from the book as a string, and returns the number of times each character, 
+# (including symbols and spaces), appears in the string.
+# Return a dictionary of letters and counts
+def count_chars(input_text):
+    #convert full text to lower case
+    lower_text = input_text.lower()
+
+    #create an empty dict for counting
+    counter_text = {}
+
+    # loop through the text and add characters/counts to the dictionary
+    for letter in lower_text:
+        if letter in counter_text:
+            counter_text[letter] +=1
+            print(counter_text[letter])
+        else:
+            counter_text[letter] = 1
+            print(counter_text[letter])
